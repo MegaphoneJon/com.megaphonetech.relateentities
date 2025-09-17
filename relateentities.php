7aa486c579dcd146796487b8bae4686cb977b425
Jon Goldberg <jon@megaphonetech.com>
2025-09-17 15:35:25 -0400

    relateentities civix update

--- relateentities.php
+++ relateentities.php
@@ -12,15 +12,6 @@ function relateentities_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function relateentities_civicrm_xmlMenu(&$files) {
-  _relateentities_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -31,21 +22,11 @@ function relateentities_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function relateentities_civicrm_postInstall() {
-  _relateentities_civix_civicrm_postInstall();
-}
-
-/**
  * Implements hook_civicrm_uninstall().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
  */
 function relateentities_civicrm_uninstall() {
-  _relateentities_civix_civicrm_uninstall();
   CRM_RelateEntities_Settings::uninstall();
 }
 
@@ -56,90 +37,6 @@ function relateentities_civicrm_uninstall() {
  */
 function relateentities_civicrm_enable() {
   _relateentities_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function relateentities_civicrm_disable() {
-  _relateentities_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function relateentities_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _relateentities_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function relateentities_civicrm_managed(&$entities) {
-  _relateentities_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function relateentities_civicrm_caseTypes(&$caseTypes) {
-  _relateentities_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function relateentities_civicrm_angularModules(&$angularModules) {
-  _relateentities_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function relateentities_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _relateentities_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function relateentities_civicrm_entityTypes(&$entityTypes) {
-  _relateentities_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function relateentities_civicrm_themes(&$themes) {
-  _relateentities_civix_civicrm_themes($themes);
 }
 
 /**
